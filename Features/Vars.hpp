ca5b5fbba385724ecdaa19e7422469d89f529c7a
TendoProject <81413625+TendoProject@users.noreply.github.com>
2024-10-20 15:20:46 +0700

    Add files via upload

---
+++ Features/Vars.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "../Include.hpp"
+
+namespace Vars
+{
+	inline bool Aimbot = true;
+	inline bool RageMode = true;
+	inline bool Speedhack = true;
+	inline bool Airstuck = true;
+}
